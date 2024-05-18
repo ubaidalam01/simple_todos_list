4f82515410dd7f61f29f5be25d4d60f517fe5a50
ubaidalam01 <ubaidm673@gmail.com>
2024-05-18 09:20:46 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todos",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Whoul you like to add more in your Todos",
+            default: "true"
+        }
+    ]);
+    todos.push(addTask.todo);
+    console.log(todos);
+    condition = addTask.addMore;
+}
